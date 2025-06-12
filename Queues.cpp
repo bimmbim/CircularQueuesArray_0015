06f3b7981021ef2adbdc8418979d458091443a3e
bimmbim <bimabaraja76@gmail.com>
2025-06-12 10:55:28 +0700

    kondisi queue kosong pada display

--- Queues.cpp
+++ Queues.cpp
@@ -57,7 +57,10 @@ class Queues {
         int FRONT_position = FRONT;
         int REAR_position = REAR;
 
-        
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
 
 
         
